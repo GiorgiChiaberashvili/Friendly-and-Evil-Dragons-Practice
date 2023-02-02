6c07374b5e6d1d159223a8f17a57ff399546c6c9
Giorgi <Gio.Chiaberashvili@gmail.com>
2023-02-02 12:28:03 +0400

    Completed

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,25 @@
 // Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...args) {
+    let Names = [];
+    args.forEach(element => {
+      if (element instanceof Dragon) {
+        Names.push(element.name)
+      }
+    })
+    return Names;
+  }
+}
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
